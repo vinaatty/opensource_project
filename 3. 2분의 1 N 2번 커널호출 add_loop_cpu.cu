ef759f9522b9bfda94ab26c8aafb20f12bc0b2e6
최예림 <127760776+vinaatty@users.noreply.github.com>
2025-04-03 14:21:58 +0900

    Update 3. 2분의 1 N 2번 커널호출 add_loop_cpu.cu

--- 3. 2분의 1 N 2번 커널호출 add_loop_cpu.cu
+++ 3. 2분의 1 N 2번 커널호출 add_loop_cpu.cu
@@ -1,33 +1,33 @@
 #include "../common/book.h"
 #define N 10000
 
-void add(int *a, int *b, int *c, int size) {
-    int tid = 0;
-    while (tid < size) {
+void add(int *a, int *b, int *c, int start, int end) {
+    int tid = start;
+    while (tid < end) {
         c[tid] = a[tid] + b[tid];
         tid += 1;
     }
 }
 
 int main(void) {
-    int a_1[N/2], b_1[N/2], c_1[N/2];
-    int a_2[N/2], b_2[N/2], c_2[N/2];
-
-    for (int i = 0; i < N/2; i++) {
-        a_1[i] = -i;
-        b_1[i] = i * i;
-        a_2[i] = -(i + N/2);
-        b_2[i] = (i + N/2) * (i + N/2);
+    int a[N], b[N], c[N];
+    for (int i = 0; i < N; i++) {
+        a[i] = -i;
+        b[i] = i * i;
     }
 
     clock_t start = clock();
-
-    add(a_1, b_1, c_1, N/2);
-    add(a_2, b_2, c_2, N/2);
-
+    add(a, b, c, 0, N / 2);     // 첫 번째 절반 연산
+    add(a, b, c, N / 2, N);     // 두 번째 절반 연산
     clock_t end = clock();
 
-    printf("소요 시간: %lf 초\n", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("소요시간: %lf\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    // 결과 확인
+    for (int i = 0; i < 10; i++) {  // 처음 10개만 출력
+        printf("%d + %d = %d\n", a[i], b[i], c[i]);
+    }
 
     return 0;
 }
+
